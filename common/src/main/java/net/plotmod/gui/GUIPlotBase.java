98097ef058105c8cd193a5843212086da12a7a87
020306 <13714905659@163.com>
2026-03-10 17:59:12 +0800

    1

---
+++ common/src/main/java/net/plotmod/gui/GUIPlotBase.java
@@ -0,0 +1,13 @@
+package net.plotmod.gui;
+
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+
+public class GUIPlotBase extends Screen {
+
+    public GUIPlotBase(Component component)
+    {
+        super(component);
+    }
+
+}
